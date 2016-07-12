5273e0588281b4c19e37d24df414b0ec2413b056
BJ Herrington <bob@dreagis.com>
2016-07-11 21:54:17 -0500

    prep nativewindow message handlers

---
+++ ux.winforms/Api/Delegates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paradoxlost.UX.WinForms.Api
+{
+	public delegate bool MessageAction(NativeWindow hwnd, ref Message m);
+}
--- ux.winforms/Api/NativeWindowHook.cs
+++ ux.winforms/Api/NativeWindowHook.cs
@@ -7,13 +7,17 @@ using System.Windows.Forms;
 
 namespace Paradoxlost.UX.WinForms.Api
 {
+	using ActionDictionary = MultiValueDictionary<WindowMessage, MessageAction>;
+
 	internal class NativeWindowHook : NativeWindow
 	{
 		protected Control Hooked { get; private set; }
+		protected ActionDictionary MessageActions { get; private set; }
 
 		public NativeWindowHook(Control control)
 			: base()
 		{
+			MessageActions = new ActionDictionary();
 			Hooked = control;
 			if (!control.IsHandleCreated)
 			{
@@ -25,6 +29,11 @@ namespace Paradoxlost.UX.WinForms.Api
 			}
 		}
 
+		public void HandleMessage(WindowMessage msg, MessageAction action)
+		{
+			this.MessageActions.Add(msg, action);
+		}
+
 		private void OnHookedHandleCreated(object sender, EventArgs e)
 		{
 			AssignHandle(Hooked.Handle);
@@ -33,16 +42,38 @@ namespace Paradoxlost.UX.WinForms.Api
 
 		protected override void WndProc(ref Message m)
 		{
-			switch ((WindowMessage)m.Msg)
+			WindowMessage msg = (WindowMessage)m.Msg;
+
+			bool messageHandled = false;
+			IReadOnlyCollection<MessageAction> actions;
+			if (this.MessageActions.TryGetValue(msg, out actions))
+			{
+				foreach (MessageAction handler in actions)
+				{
+					try
+					{
+						messageHandled |= handler(this, ref m);
+					}
+					catch
+					{
+					}
+				}
+			}
+
+			switch (msg)
 			{
 				case WindowMessage.Close:
 				case WindowMessage.Destroy:
+					this.MessageActions.Clear();
 					this.ReleaseHandle();
 					Hooked = null;
 					break;
 
 				default:
-					base.WndProc(ref m);
+					if (!messageHandled)
+					{
+						base.WndProc(ref m);
+					}
 					break;
 			}
 		}
