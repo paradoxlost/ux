865be1865ffc6edc1fab76b738cad70758da50d3
BJ Herrington <bob@dreagis.com>
2017-03-27 01:34:02 -0500

    nuget package

--- ux.winforms/Properties/AssemblyInfo.cs
+++ ux.winforms/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ux.winforms")]
+[assembly: AssemblyTitle("Paradoxlost WinForms Theme Engine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ux.winforms")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Paradoxlost")]
+[assembly: AssemblyProduct("Paradoxlost UX")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
