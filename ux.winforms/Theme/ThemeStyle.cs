1f10be668e47b9262e2c52c10130c0080ca5d592
Ofir Winegarten <ofirw@gotechvision.com>
2018-09-20 13:38:46 +0300

    A bit of refactoring and supprort for primitive types

--- ux.winforms/Theme/ThemeStyle.cs
+++ ux.winforms/Theme/ThemeStyle.cs
@@ -96,70 +96,86 @@ namespace Paradoxlost.UX.WinForms.Theme
                 Type propertyType = pi.PropertyType;
                 string[] args = pairValue.Split(new string[] { ", " }, StringSplitOptions.None);
 
-                // color is stupid
-                // it doesn't use ctors. it has a bunch of static methods
+
                 if (propertyType == typeof(Color))
                 {
-                    object value;
-                    // we'll only handle names & html
-                    if (args[0][0] == '#')
-                    {
-                        value = ColorTranslator.FromHtml(args[0]);
-                    }
-                    else
-                    {
-                        value = Color.FromKnownColor(
-                            (KnownColor)Enum.Parse(typeof(KnownColor), args[0], true));
-                    }
-                    pi.SetValue(control, value, null);
-                    //pi.SetValue(control, value);
-
-                    continue;
+                    SetColor(control, pi, args);
                 }
-
-                // find a property ctor with the same number of args
-                foreach (ConstructorInfo ci in propertyType.GetConstructors())
+                else if (propertyType.IsPrimitive)
                 {
-                    ParameterInfo[] ctorArgs = ci.GetParameters();
-                    if (ctorArgs.Length != args.Length)
-                        continue;
-
-                    // see if we can use this one
-                    object[] values = new object[args.Length];
-                    bool good = true;
-                    for (int i = 0; i < ctorArgs.Length; i++)
-                    {
-                        try
-                        {
-                            Type argType = ctorArgs[i].ParameterType;
-                            if (argType.IsEnum)
-                            {
-                                values[i] = Enum.Parse(argType, args[i], true);
-                            }
-                            else
-                            {
-                                values[i] = Convert.ChangeType(args[i], ctorArgs[i].ParameterType);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            if (ex is ArgumentException || ex is ArgumentNullException || ex is InvalidCastException)
-                            {
-                                good = false;
-                                break;
-                            }
-                            throw;
-                        }
-                    }
-
-                    if (good)
-                    {
-                        object value = ci.Invoke(values);
-                        pi.SetValue(control, value, null);
-                        break;
-                    }
+                    var value = Convert.ChangeType(args[0], propertyType);
+                    pi.SetValue(control, value, null);
+                }
+                else
+                {
+                    SetViaCtor(control, pi, propertyType, args);
                 }
             }
+        }
+
+        private static void SetViaCtor(Control control, PropertyInfo pi, Type propertyType, string[] args)
+        {
+            // find a property ctor with the same number of args
+            foreach (ConstructorInfo ci in propertyType.GetConstructors())
+            {
+                ParameterInfo[] ctorArgs = ci.GetParameters();
+                if (ctorArgs.Length != args.Length)
+                    continue;
+
+                // see if we can use this one
+                object[] values = new object[args.Length];
+                bool good = true;
+                for (int i = 0; i < ctorArgs.Length; i++)
+                {
+                    try
+                    {
+                        Type argType = ctorArgs[i].ParameterType;
+                        if (argType.IsEnum)
+                        {
+                            values[i] = Enum.Parse(argType, args[i], true);
+                        }
+                        else
+                        {
+                            values[i] = Convert.ChangeType(args[i], ctorArgs[i].ParameterType);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex is ArgumentException || ex is ArgumentNullException || ex is InvalidCastException)
+                        {
+                            good = false;
+                            break;
+                        }
+                        throw;
+                    }
+                }
+
+                if (good)
+                {
+                    object value = ci.Invoke(values);
+                    pi.SetValue(control, value, null);
+                    break;
+                }
+            }
+        }
+
+        private static void SetColor(Control control, PropertyInfo pi, string[] args)
+        {
+            // color is stupid
+            // it doesn't use ctors. it has a bunch of static methods
+            object value;
+            // we'll only handle names & html
+            if (args[0][0] == '#')
+            {
+                value = ColorTranslator.FromHtml(args[0]);
+            }
+            else
+            {
+                value = Color.FromKnownColor(
+                    (KnownColor)Enum.Parse(typeof(KnownColor), args[0], true));
+            }
+            pi.SetValue(control, value, null);
+            //pi.SetValue(control, value);
         }
     }
 }
